b044321b67623103259cb1038d1a46ec315bb2be
gonzonia <36857621+gonzonia@users.noreply.github.com>
2023-07-04 16:19:27 -0400

    Update node_helper.js
    Update to use needle instead of request.

--- node_helper.js
+++ node_helper.js
@@ -5,8 +5,11 @@
  * Updated to used Pirateweather by Gonzonia
  * MIT Licensed
  */
-const NodeHelper = require('node_helper');
-const request = require('request');
+
+
+var NodeHelper = require("node_helper");
+var needle = require("needle");
+var moment = require("moment");
 
 
 
@@ -17,7 +20,26 @@ module.exports = NodeHelper.create({
     },
 
     getWeather: function(url) {
-        request({
+        
+		var self = this;
+		 needle.get(url, function(error, response, body) {
+         if (!error){
+  			if(response.statusCode == 200) {
+            	//Good response
+				var result = body; //needle automagically parses the response as JSON
+				//console.log(body);
+                result.instanceId = url.instanceId;
+                self.sendSocketNotification("WEATHER_RESULT", result);
+               }
+               else {
+               	console.error("[MMM-BMW-PW] " + moment().format("D-MMM-YY HH:mm") + " ** unexpected response code ** " + response.statusCode);
+                }
+		 	} else {
+                console.error("[MMM-BMW-PW] " + moment().format("D-MMM-YY HH:mm") + " ** ERROR ** " + error);
+            }
+		});	 
+		/*		
+		request({
             url: url,
             method: 'GET'
         }, (error, response, body) => {
@@ -26,9 +48,8 @@ module.exports = NodeHelper.create({
 //					console.log(response.statusCode); // for checking
                     this.sendSocketNotification('WEATHER_RESULT', result);
             }
-        });
-    },
-
+        ;*/
+     },
     socketNotificationReceived: function(notification, payload) {
         if (notification === 'GET_WEATHER') {
             this.getWeather(payload);
